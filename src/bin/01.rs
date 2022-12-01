3ee5907229e42d1dae95fbd2ca8797f70f1db2aa
eba <eba@silversolutions.de>
2022-12-01 11:39:58 +0100

    day 1 - part 1

---
+++ src/bin/01.rs
@@ -0,0 +1,38 @@
+pub fn part_one(input: &str) -> Option<u32> {
+    input
+        .split("\n\n")
+        .map(|items| items
+            .lines()
+            .map(|num| num.parse::<u32>().unwrap())
+            .reduce(|acc, item| acc + item)
+            .unwrap()
+        )
+        .max()
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    None
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 1);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_one(&input), Some(24000));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 1);
+        assert_eq!(part_two(&input), None);
+    }
+}
