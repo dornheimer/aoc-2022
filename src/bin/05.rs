021438d471538e38dd0e2bcaf1957feac21996a1
eba <eba@silversolutions.de>
2022-12-05 18:19:04 +0100

    day 5 - no regex

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,6 +1,4 @@
 use itertools::Itertools;
-use lazy_static::lazy_static;
-use regex::Regex;
 use std::str::FromStr;
 
 #[derive(Debug)]
@@ -14,18 +12,16 @@ impl FromStr for Instruction {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        lazy_static! {
-            static ref RE: Regex = Regex::new(r"move (\d+) from (\d+) to (\d+)").unwrap();
-        }
+        // example: move[0] 2[1] from[2] 2[3] to[4] 3[5]
+        let parts: Vec<_> = s.split_ascii_whitespace().collect();
 
-        let captures = RE.captures(s).unwrap();
-        let from = (&captures[2]).parse::<usize>().unwrap();
-        let to = (&captures[3]).parse::<usize>().unwrap();
+        let from = parts[3].parse::<usize>().unwrap();
+        let to = parts[5].parse::<usize>().unwrap();
 
         Ok(Instruction {
             from: from - 1,
             to: to - 1,
-            amount: (&captures[1]).parse().unwrap(),
+            amount: parts[1].parse().unwrap(),
         })
     }
 }
@@ -59,28 +55,28 @@ pub fn part_two(input: &str) -> Option<String> {
     Some(get_top_crates(supplies))
 }
 
-fn parse_stacks(input: &str) -> Vec<Vec<&str>> {
+fn parse_stacks(input: &str) -> Vec<Vec<char>> {
     let mut stack_iter = input.lines().rev();
     let no_stacks = stack_iter
         .next()
         .map(|l| l.trim().chars().filter(|c| !c.is_whitespace()).count())
         .unwrap();
 
-    let mut stacks: Vec<Vec<&str>> = vec![vec![]; no_stacks];
+    let mut stacks = vec![vec![]; no_stacks];
     stack_iter.for_each(|row| {
         get_stacks(row)
             .iter()
             .enumerate()
             .for_each(|(ix, cargo)| match cargo {
                 None => (),
-                Some(cargo) => stacks[ix].push(cargo),
+                Some(cargo) => stacks[ix].push(*cargo),
             })
     });
 
     stacks
 }
 
-fn get_top_crates(supplies: Vec<Vec<&str>>) -> String {
+fn get_top_crates(supplies: Vec<Vec<char>>) -> String {
     supplies.iter().filter_map(|stack| stack.last()).join("")
 }
 
@@ -90,14 +86,18 @@ fn main() {
     advent_of_code::solve!(2, part_two, input);
 }
 
-fn get_stacks(row: &str) -> Vec<Option<&str>> {
-    lazy_static! {
-        static ref RE: Regex = Regex::new(r"(\s{3})\s?|\[(\w)?]\s?").unwrap();
-    }
+fn get_stacks(row: &str) -> Vec<Option<char>> {
+    let mut stack: Vec<Option<char>> = vec![];
+    row.chars().skip(1).enumerate().for_each(|(ix, c) | {
+       if ix % 4 == 0 {
+           stack.push(match c.is_whitespace() {
+               true => None,
+               false => Some(c)
+           });
+       }
+    });
 
-    RE.captures_iter(row)
-        .map(|c| c.get(2).map_or(None, |m| Some(m.as_str())))
-        .collect()
+    stack
 }
 
 #[cfg(test)]
@@ -118,13 +118,13 @@ mod tests {
 
     #[test]
     fn test_get_stacks() {
-        assert_eq!(get_stacks("[Z] [M]    "), vec![Some("Z"), Some("M"), None]);
-        assert_eq!(get_stacks("[Z]        "), vec![Some("Z"), None, None]);
-        assert_eq!(get_stacks("    [M] [P]"), vec![None, Some("M"), Some("P")]);
-        assert_eq!(get_stacks("        [P]"), vec![None, None, Some("P")]);
+        assert_eq!(get_stacks("[Z] [M]    "), vec![Some('Z'), Some('M'), None]);
+        assert_eq!(get_stacks("[Z]        "), vec![Some('Z'), None, None]);
+        assert_eq!(get_stacks("    [M] [P]"), vec![None, Some('M'), Some('P')]);
+        assert_eq!(get_stacks("        [P]"), vec![None, None, Some('P')]);
         assert_eq!(
             get_stacks("[Z] [M] [P]"),
-            vec![Some("Z"), Some("M"), Some("P")]
+            vec![Some('Z'), Some('M'), Some('P')]
         );
     }
 }
